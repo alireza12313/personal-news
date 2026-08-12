157dfb5672e2b071d224d45086d94604602232b6
alireza12313 <alirezaadibi12313@gmail.com>
2026-08-12 11:54:59 +0330

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,6 @@ app.post("/newsletter", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
     console.log("Server is running on port 3000");
 });
